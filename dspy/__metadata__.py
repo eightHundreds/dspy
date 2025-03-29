b74bef48e4919e33a284c7f096ac551dbaf4eb75
游嘉明 <youjiaming1995@outlook.com>
2025-03-29 22:18:24 +0800

    feat: 更新元数据，修改版本号为2.6.16-beta

--- dspy/__metadata__.py
+++ dspy/__metadata__.py
@@ -1,8 +1,8 @@
-#replace_package_name_marker
-__name__="dspy"
-#replace_package_version_marker
-__version__="2.6.16"
-__description__="DSPy (eightHundreds fork)"
-__url__="https://github.com/eightHundreds/dspy"
-__author__="eightHundreds"
-__author_email__="eightHundreds@example.com"
+# replace_package_name_marker
+__name__ = "dspy"
+# replace_package_version_marker
+__version__ = "2.6.16-beta"
+__description__ = "DSPy (eightHundreds fork)"
+__url__ = "https://github.com/eightHundreds/dspy"
+__author__ = "eightHundreds"
+__author_email__ = "eightHundreds@example.com"
